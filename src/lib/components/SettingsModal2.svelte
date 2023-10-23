5fe131d149ffc9b2b8fc7c557966e530a48dc6c8
lyie28 <laurayie93@gmail.com>
2023-10-23 17:22:44 +0200

    updating styling

--- src/lib/components/SettingsModal2.svelte
+++ src/lib/components/SettingsModal2.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
-	import Modal from "$lib/components/Modal.svelte";
+	import Modal from "$lib/components/BigModal.svelte";
 	import { writable } from 'svelte/store';
 	import CarbonClose from "~icons/carbon/close";
-	import Switch from "$lib/components/Switch.svelte";
-	import { enhance } from "$app/forms";
-	import { base } from "$app/paths";
 	import type { Model } from "$lib/types/Model";
 	import type { LayoutData } from "../../routes/$types";
 
@@ -15,6 +12,8 @@
 	let isConfirmingDeletion = false;
 	let isAccountView: boolean = true;
 	let isThemeView: boolean = false;
+	let isEmailUpdateSuccessful = false;
+	let newEmail = '';
 	const themeStore = writable(
 		localStorage.theme
 	);
@@ -50,84 +49,62 @@
 				method: "POST",
 			});
 		}
-</script>
+	
+	async function changeEmail(newEmail: string) {
+    try {
+      const response = await fetch('/auth/changeEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ new_email: newEmail }),
+      });
 
-<style>
-	/* Yellow Button Style */
-	.yellow-button {
-		padding: 8px 12px; /* top-bottom, left-right */
-		background-color: #f0ba2d;
-		min-width: 150px;
-		color: black;
-		border-radius: 8px; /* adjust for desired roundness */
-	}
-
-	.delete-button {
-		padding: 8px 12px; /* top-bottom, left-right */
-		border-radius: 8px; /* adjust for desired roundness */
-		min-width: 150px;
-	}
-
-	/* Input Box Style */
-	.input-box {
-		flex: 1;
-		padding: 8px; /* Internal padding for input */
-		border: 1px solid gray;
-		width: 60%; /* reduce the width */
-		max-width: 300px; /* or adjust to desired max width */
-	}
-
-	/* Flex container for Delete account heading and button */
-	.delete-container, .flex-container {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		flex-wrap: wrap; /* allows elements to wrap to next line if they don't fit */
-		gap: 10px; /* space between flex children */
-	}
-
-	.delete-container {
-		padding: 20px 0;
-	}
-
-	.create-pass-btn {
-		align-items: right;
-	}
-
-	#accountView {
-		padding-left: 20px; /* Left padding for the AccountView */
-	}
-
-	/* Padding for H2 headers */
-	h2 {
-		padding-top: 30px; /* or any other value you prefer */
-		font-weight:500;
-		font-size:larger;
-	}
-
-	/* Delete Button Style */
-	.delete-button {
-		border: 1px solid red;
-		margin-left: auto; /* push it to the right */
-	}
-
-	.delete-button:hover {
-		background-color: red;
-		color: white; /* assuming text color should be white on red background */
-	}
-
-	t1 {
-		color: white;
-		font-size: 24px;
-		padding: 15px 0;
-	}
-
-	.modal-content-theme {
-        display: flex;
-        padding: 20px 20px; /* Padding above the button. Adjust value as needed. */
+      if (response.ok) {
+      } else {
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
-	</style>
+	}
 
+	async function changePassword(newPassword: string, confirmPassword: string) {
+    try {
+      if (newPassword !== confirmPassword) {
+        console.error('Password mismatch');
+        return;
+    	}
+
+      	const response = await fetch('/auth/changePassword', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ new_password: newPassword }),
+      });
+
+      if (response.ok) {
+        // Handle a successful response here, e.g., show a success message or redirect
+      } else {
+        // Handle an error response here, e.g., show an error message
+      }
+    } catch (error) {
+      // Handle any network or request error here
+      console.error('Error:', error);
+    }
+  }
+
+  let newPassword = '';
+  let confirmPassword = '';
+
+  async function handlePasswordChange() {
+    await changePassword(newPassword, confirmPassword);
+  }
+
+  async function handleEmailChange() {
+    	await changeEmail(newEmail);
+	}
+</script>
 
 <Modal on:close>
 	<script>
@@ -137,23 +114,23 @@
 	  <Overlay bind:this={overlayComp} />
 	  overlayComp.setTheme();
 	</script>
-	<div class="header" style="position: relative; display: flex; min-width:700px!important; font-family: Sora,sans-serif; background-color:#111827; text-align:center; justify-content: center; align-items: center; padding: 0 15px;">
+	<div class="bg-white dark:bg-darkBackground dark:text-white dark:border-b-0 border-b border-b-gray" style="position: relative; display: flex; min-width:700px!important; font-family: Sora,sans-serif; text-align:center; justify-content: center; align-items: center; padding: 0 15px;">
 		{#if isAccountView}
-			<t1>Account</t1>
+			<div class="text-2xl py-4 dark:text-white">Account</div>
 		{:else}
-			<t1>Theme</t1>
+			<div class="text-2xl py-4 dark:text-white">Theme</div>
 		{/if}
 		
-		<button type="button" class="group" on:click={() => dispatch("close")} style="position: absolute; right: 15px; color: white;">
+		<button type="button" class="group text-black dark:text-white" on:click={() => dispatch("close")} style="position: absolute; right: 15px;">
 			<CarbonClose />
 		</button>
 	</div>
 	
-	<div id="modal" class="dark:text-white" style="display: flex; min-width:700px!important; font-family: Sora,sans-serif; background-color:#111827">
+	<div id="modal" class="bg-white dark:bg-darkBackground dark:text-white" style="display: flex; min-width:700px!important; font-family: Sora,sans-serif;">
 		<!-- Left-hand side menu -->
-		<div class="side-menu" style="flex:1; display: flex; flex-direction: column; background-color: #131a28;">
-			<button class="group flex h-11 flex-none items-center gap-1.5 rounded-lg pl-3 pr-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700" id="accountButton" on:click={accountViewToggle} type="button">Account</button>
-			<button class="group flex h-11 flex-none items-center gap-1.5 rounded-lg pl-3 pr-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700" id="themeButton" on:click={themeViewToggle} type="button">Theme</button>
+		<div class="dark:border-r-0 border-r border-r-gray bg-lightSidebar dark:bg-darkSidebar" style="flex:1; display: flex; flex-direction: column;">
+			<button class="group flex h-11 flex-none items-center gap-1.5 rounded-lg pl-3 pr-2 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-400" id="accountButton" on:click={accountViewToggle} type="button">Account</button>
+			<button class="group flex h-11 flex-none items-center gap-1.5 rounded-lg pl-3 pr-2 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-gray-400" id="themeButton" on:click={themeViewToggle} type="button">Theme</button>
 		</div>
 	
 		<div class = views style="flex: 4; min-width:500px important; min-height:400px; margin-right:40px; padding 15px 15px;">
@@ -162,35 +139,35 @@
 	<script type="text/javascript">
 		document.getElementById("themeView").style.display = "block";
 	</script>
-	<div class="modal-content" id="accountView"> 
+		<div class="modal-content pl-5" id="accountView"> 
 		<!-- Change email section -->
-		<h2 style="color: #f0ba2d;">Change email</h2>
-		<div class="flex-container">
-			<input type="text" placeholder="email@example.com" class="input-box">
-			<button class="yellow-button">Change email</button>
+		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Change email</h2>
+		<p>New email address</p>
+		<div class="flex justify-between items-center flex-wrap gap-2.5 border-1 border-black dark:border-gray">
+    	<input type="email" placeholder="Enter new email address" class="p-2 w-3/5 border-1 border-black dark:border-gray max-w-xs flex-1" bind:value={newEmail} style="border: 1px solid black;" />
+    	<button class="bg-yellow-500 text-black rounded-lg min-w-36 py-2 px-3" style="justify-content: space-between;" on:click={handleEmailChange}>Change Email</button>
 		</div>
 
 		<!-- Create password section -->
-		<h2 style="color: #f0ba2d;">Create password</h2>
+		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Create password</h2>
 		<p>New password</p>
-		<input type="password" placeholder="Enter new password" class="input-box">
+		<input type="password" placeholder="Enter new password" class="p-2 w-3/5 max-w-xs flex-1" style="border: 1px solid black;" >
 		<p style="padding-top:20px;">Confirm password</p>
-		<div class="flex-container">
-		<input type="password" placeholder="Confirm new password" class="input-box" style="justify-content: space-between;">
-		<button class="yellow-button" style="justify-content: space-between;">Create password</button>
+		<div class="border-1 border-black dark:border-gray flex justify-between items-center flex-wrap gap-2.5">
+		<input type="password" placeholder="Confirm new password" class="p-2 w-3/5 max-w-xs flex-1" style="justify-content: space-between; border: 1px solid black;" bind:value={confirmPassword}/>
+		<button class="bg-yellow-500 text-black rounded-lg min-w-36 py-2 px-3" style="justify-content: space-between;" on:click={handlePasswordChange}>Create password</button>
 		</div>
-		
 
 		<!-- Delete account section -->
-		<h2 style="color: #f0ba2d;">Delete account</h2>
-		<div class="delete-container">
+		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Delete account</h2>
+		<div class="flex justify-between items-center flex-wrap gap-2.5 py-5">
 			<button on:click = {() => (isConfirmingDeletion = true)}
-				class="delete-button"> Delete account</button>
+				class="rounded-lg min-w-36 py-2 px-3 ml-auto border-1 border-red :hover bg-red-700 text-white"> Delete account</button>
 			{#if isConfirmingDeletion}
 			<Modal on:close={() => (isConfirmingDeletion = false)}>
 				<form class="flex w-full flex-col gap-5 p-6">
 					<div class="flex items-start justify-between text-xl font-semibold text-gray-800">
-						<h2>Are you sure?</h2>
+						<h2 class="pt-8 font-medium text-xl">Are you sure?</h2>
 						<button type="button" class="group" on:click={() => (isConfirmingDeletion = false)}>
 							<CarbonClose class="text-gray-900 group-hover:text-gray-500" />
 						</button>
@@ -218,7 +195,7 @@
 			document.getElementById("themeView").style.display = "block";
 		</script>
 		<!-- Theme modal view -->
-        <div class="modal-content-theme" id="themeView">
+        <div class="flex p-5" id="themeView">
         <button
 		on:click={switchTheme}
 		id="darkButton"
