6ceef46c678df2a65a0285093b943d56b8626e6f
lyie28 <laurayie93@gmail.com>
2023-10-24 09:25:12 +0200

    front end updates

--- src/lib/components/SettingsModal2.svelte
+++ src/lib/components/SettingsModal2.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import Modal from "$lib/components/BigModal.svelte";
+	import Modal2 from "$lib/components/straightModal.svelte";
 	import TextModal from "$lib/components/TextModal.svelte";
 	import { writable } from 'svelte/store';
 	import CarbonClose from "~icons/carbon/close";
@@ -12,10 +13,12 @@
 
 	let opSuccess = false;
 	let opFail = false;
+	let emailOpSuccess = false;
+	let emailOpFail = false;
+	let mismatch = false;
 	let isConfirmingDeletion = false;
 	let isAccountView: boolean = true;
 	let isThemeView: boolean = false;
-	let isEmailUpdateSuccessful = false;
 	let newEmail = '';
 	const themeStore = writable(
 		localStorage.theme
@@ -64,9 +67,9 @@
       });
 
       if (response.ok) {
-		opSuccess = true;
+		emailOpSuccess = true;
       } else {
-		opFail = true;
+		emailOpFail = true;
       }
     } catch (error) {
       console.error('Error:', error);
@@ -76,7 +79,7 @@
 	async function changePassword(newPassword: string, confirmPassword: string) {
     try {
       if (newPassword !== confirmPassword) {
-        console.error('Password mismatch');
+        mismatch = true;
         return;
     	}
 
@@ -149,27 +152,27 @@
 		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Change email</h2>
 		<p>New email address</p>
 		<div class="flex justify-between items-center flex-wrap gap-2.5 border-1 border-black dark:border-gray">
-    	<input type="email" placeholder="Enter new email address" class="p-2 w-3/5 border-1 border-black dark:border-gray max-w-xs flex-1" bind:value={newEmail} style="border: 1px solid black;" />
+    	<input type="email" placeholder="Enter new email address" class="p-2 w-3/5 border-1 border-black dark:border-gray max-w-xs flex-1" bind:value={newEmail} style="border: 1px solid black; color:black;" />
     	<button class="bg-yellow-500 text-black rounded-lg min-w-36 py-2 px-3" style="justify-content: space-between;" on:click={handleEmailChange}>Change Email</button>
-		{#if opSuccess}
-			<TextModal title="Operation succesful" text="Your email has succesfully been updated ✅"  on:close={() => (opSuccess = false)}/>
-		{:else if opFail}
-			<TextModal title="Operation not succesful" text="Please check your email address match and are valid" on:close={() => (opFail = false)}/>
+		{#if emailOpSuccess}
+			<TextModal title="Email updated" text="✅ Your email has successfully been updated"  on:close={() => (emailOpSuccess = false)}/>
+		{:else if emailOpFail}
+			<TextModal title="Update failed" text="Please check the email address entered is valid" on:close={() => (emailOpFail = false)}/>
 		{/if}
 		</div>
 
 		<!-- Create password section -->
 		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Create password</h2>
 		<p>New password</p>
-		<input type="password" placeholder="Enter new password" class="p-2 w-3/5 max-w-xs flex-1" style="border: 1px solid black;" >
+		<input type="password" placeholder="Enter new password" class="p-2 w-3/5 max-w-xs flex-1" style="border: 1px solid black; color:black;" >
 		<p style="padding-top:20px;">Confirm password</p>
 		<div class="border-1 border-black dark:border-gray flex justify-between items-center flex-wrap gap-2.5">
-		<input type="password" placeholder="Confirm new password" class="p-2 w-3/5 max-w-xs flex-1" style="justify-content: space-between; border: 1px solid black;" bind:value={confirmPassword}/>
+		<input type="password" placeholder="Confirm new password" class="p-2 w-3/5 max-w-xs flex-1" style="justify-content: space-between; border: 1px solid black; color:black;" bind:value={confirmPassword}/>
 		<button class="bg-yellow-500 text-black rounded-lg min-w-36 py-2 px-3" style="justify-content: space-between;" on:click={handlePasswordChange}>Create password</button>
 		{#if opSuccess}
-			<TextModal title="Operation succesful" text="Your email has succesfully been updated ✅"  on:close={() => (opSuccess = false)}/>
+			<TextModal title="Password updated" text="✅ Your password has successfully been created"  on:close={() => (opSuccess = false)}/>
 		{:else if opFail}
-			<TextModal title="Operation not succesful" text="Please check your email address match and are valid" on:close={() => (opFail = false)}/>
+			<TextModal title="Update failed" text={mismatch === true ? "Passwords don't match" : "Unexpected error. Please try again"} on:close={() => (opFail = false, mismatch = false)}/>
 		{/if}
 		</div>
 
@@ -177,20 +180,17 @@
 		<h2 class="pt-8 font-medium text-xl" style="color: #f0ba2d;">Delete account</h2>
 		<div class="flex justify-between items-center flex-wrap gap-2.5 py-5">
 			<button on:click = {() => (isConfirmingDeletion = true)}
-				class="rounded-lg min-w-36 py-2 px-3 ml-auto border-1 border-red :hover bg-red-700 text-white"> Delete account</button>
+				class="rounded-lg min-w-36 py-2 px-3 ml-auto border-1 border-red :hover bg-red-700 text-white">Delete account</button>
 			{#if isConfirmingDeletion}
-			<Modal on:close={() => (isConfirmingDeletion = false)}>
-				<form class="flex w-full flex-col gap-5 p-6">
+			<Modal2 on:close={() => (isConfirmingDeletion = false)}>
+				<form class="dark:bg-darkBackground flex w-full flex-col gap-5 p-6 dark:border dark:border-gray-400">
 					<div class="flex items-start justify-between text-xl font-semibold text-gray-800">
-						<h2 class="pt-8 font-medium text-xl">Are you sure?</h2>
+						<h2 class="dark:text-white font-medium text-xl">Are you sure?</h2>
 						<button type="button" class="group" on:click={() => (isConfirmingDeletion = false)}>
-							<CarbonClose class="text-gray-900 group-hover:text-gray-500" />
+							<CarbonClose class="text-gray-900 dark:text-gray-400 group-hover:text-gray-500"/>
 						</button>
 					</div>
-					<p class="text-gray-800">
-						This action will delete your account.<br>
-						This cannot be undone.
-					</p>
+					<p class="dark:text-gray-400 text-gray-800">This action will delete your account.<br>This cannot be undone.</p>
 					<button
 						type="button"
 						class="mt-2 rounded-full bg-red-700 px-5 py-2 text-lg font-semibold text-gray-100 ring-gray-400 ring-offset-1 transition-all hover:ring focus-visible:outline-none focus-visible:ring"
@@ -199,7 +199,7 @@
 						Confirm
 					</button>
 				</form>
-			</Modal>
+			</Modal2>
 		{/if}
 		</div>
 	</div>
--- src/lib/components/TextModal.svelte
+++ src/lib/components/TextModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Modal from "$lib/components/Modal.svelte";
+	import Modal from "$lib/components/straightModal.svelte";
 	import { createEventDispatcher } from "svelte";
 	import CarbonClose from "~icons/carbon/close";
 
@@ -9,16 +9,15 @@
 </script>
 
 <Modal>
-	<div class="border border-gray flex w-full flex-col gap-0 p-2 dark:bg-darkBackground dark:text-white" style="border-radius: 0; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-}">
+	<div class="dark:bg-darkBackground border border-gray-600 p-2">
 		<div class="dark:text-white m-2 flex items-start text-xl font-bold text-gray-800" style="justify-content: space-between;">
-			<h1 class="dark:text-white">{title}</h1>
+			<h2>{title}</h2>
 			<br />
 			<button type="button" class="group" on:click={() => dispatch("close")}>
 				<CarbonClose class="dark:text-white text-gray-900 group-hover:text-gray-500" />
 			</button>
 		</div>
-		<div class="dark:text-white text-m m-2 flex items-start text-gray-800">
+		<div class="dark:text-gray-400 text-m m-2 text-gray-800">
 			<br/>{text}
 		</div>
 	</div>
