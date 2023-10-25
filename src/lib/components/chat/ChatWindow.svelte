f58b954d4c3c94db927e9eb6b2ba35a0a94afbff
JoFrost <mebessaa@student.42.fr>
2023-10-25 14:56:07 +0000

    style updates

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -47,8 +47,8 @@
 </script>
 
 <div class="relative min-h-0 min-w-0">
-	<div class="bg-bannerBack dark:bg-gray-800 dark:border-0 border-b" display="none">
-		<p class="text-black/white" id="banner" style="text-align: center; display: none;">
+	<div class="justify-center text-center items-center flex bg-bannerBack dark:bg-chat dark:border-0 border-b">
+		<p class="p-3 text-black/white">
 			🔒 Prompts are end-to-end protected.<br
 			/>Not even Mithril Security can read or train on them. Learn more 
 			<a
@@ -74,7 +74,8 @@
 			if (!loading) dispatch("retry", ev.detail);
 		}}
 	/>
-	<div
+</div>
+<div
 		class="dark:via-gray-80 pointer-events-none absolute inset-x-0 bottom-0 z-0 mx-auto flex w-full max-w-3xl flex-col items-center justify-center bg-gradient-to-t from-white via-white/80 to-white/0 px-3.5 py-4 dark:border-gray-800 dark:from-gray-900 dark:to-gray-900/0 max-md:border-t max-md:bg-white max-md:dark:bg-gray-900 sm:px-5 md:py-8 xl:max-w-4xl [&>*]:pointer-events-auto"
 	>
 		<div class="flex w-full pb-3 max-md:justify-between">
