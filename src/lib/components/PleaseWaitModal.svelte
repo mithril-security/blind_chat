77a6cfd91b4c0ddf4f6182ab55ba858911c72403
JoFrost <mebessaa@student.42.fr>
2023-10-26 15:08:13 +0000

    feat: email change working & email showing up

---
+++ src/lib/components/PleaseWaitModal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Modal from "$lib/components/Modal.svelte";
+	import { createEventDispatcher } from "svelte";
+	import CarbonClose from "~icons/carbon/close";
+
+	const dispatch = createEventDispatcher<{ close: void }>();
+</script>
+
+<Modal>
+	<div class="flex w-full flex-col gap-0 p-2">
+		<div class="m-2 flex items-start text-xl font-bold text-gray-800">
+			<h1>Please wait...</h1>
+			<br />
+		</div>
+		<br />
+	</div>
+</Modal>
