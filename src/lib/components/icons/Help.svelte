6a21bebbbd8283ea5a99353c4c5aaf0a778d3293
JoFrost <mebessaa@student.42.fr>
2023-10-27 11:46:48 +0000

    work in progress help menu

---
+++ src/lib/components/icons/Help.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { PUBLIC_APP_ASSETS, PUBLIC_APP_NAME, PUBLIC_ORIGIN } from "$env/static/public";
+	import { base } from "$app/paths";
+
+	export let classNames = "";
+</script>
+
+<object
+	class={classNames}
+	data="{PUBLIC_ORIGIN || $page.url.origin}{base}/{PUBLIC_APP_ASSETS}/help.png"
+	title="help button"
+	width="20"  
+	height="20" 
+/>
