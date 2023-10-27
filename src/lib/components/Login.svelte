231a942b5bd5b0e9ebac57bbffb8063e0920966e
JoFrost <mebessaa@student.42.fr>
2023-10-27 15:54:40 +0000

    update

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
+    import { PUBLIC_APP_ASSETS, PUBLIC_ORIGIN } from "$env/static/public";
+    import { page } from "$app/stores";
     import TextModal from "$lib/components/TextModal.svelte";
     import { Checkbox } from "svelte-mui";
 	import Modal from "$lib/components/BigModal.svelte";
@@ -240,28 +242,26 @@
             />
             {#if !showPassword}
              <!-- show password icon -->    
-            <svg 
-                class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv toggle-visibility-icon absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                focusable="false" 
-                aria-hidden="true" 
-                viewBox="0 0 24 24" 
-                on:click={handleClickShowPassword}
-                style="width: 24px; height: 24px;" 
-            >
-                <path fill="#808080" d="M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z"></path>
-            </svg>
+             <img
+             class="css-vubbuv toggle-visibility-icon absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+             alt="show password button"
+             src="{PUBLIC_ORIGIN || $page.url.origin}{base}/{PUBLIC_APP_ASSETS}/show-password.png"
+             title="show password button"
+             width="20"  
+             height="20"
+             on:click={handleClickShowPassword}
+            />
             {:else}
              <!-- TODO: hide password icon -->    
-            <svg 
-                class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv toggle-visibility-icon absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                focusable="false" 
-                aria-hidden="true" 
-                viewBox="0 0 24 24" 
-                on:click={handleClickShowPassword}
-                style="width: 24px; height: 24px;" 
-            >
-                <path fill="#808080" d="M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z"></path>
-            </svg>
+             <img
+             class="css-vubbuv toggle-visibility-icon absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+             alt="help password button"
+             src="{PUBLIC_ORIGIN || $page.url.origin}{base}/{PUBLIC_APP_ASSETS}/hide-password.png"
+             title="hide password button"
+             width="20"  
+             height="20"
+             on:click={handleClickShowPassword}
+            />
             {/if}
         </div>
         <!-- Sign in button -->
