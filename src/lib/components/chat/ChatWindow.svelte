3f5cc8b95a9aca6537a028befdacfbd671eef05a
JoFrost <mebessaa@student.42.fr>
2023-10-31 09:20:56 +0000

    local changes

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -197,23 +197,23 @@
 		<script type="text/javascript">
 			document.getElementById("helpMenu").style.display = "block";
 		</script>
-			<div id="helpMenu" class="bottom-[45%] right-[0%] md:bottom-[45%] md:right-[0%] xl:bottom-[50%] xl:right-[-18%] py-2 border border-gray-600 flex justify-center items-center rounded-2xl bg-darkBackground {isHelpMenuOpen ? 'open' : ''}" style="position: absolute;">
+			<div id="helpMenu" class="bottom-[45%] right-[0%] md:bottom-[45%] md:right-[0%] xl:bottom-[50%] xl:right-[-18%] pt-2 border border-gray-600 flex justify-center items-center rounded-2xl bg-[#0d1830] {isHelpMenuOpen ? 'open' : ''}" style="position: absolute;">
 				<div>
 					<div class = "flex justify-end">
 						<button type="button" class="pr-2 justify-end" on:click={closeHelpMenu}>
 							<CarbonClose class="justify-end text-white" />
 						</button>
 					</div>
-					<a href="https://www.mithrilsecurity.io/contact" target="_blank" rel="noopener noreferrer" class="px-8 block py-3 text-center h-11 text-white hover:bg-gray-600" style="width: 100%;">
+					<a href="https://www.mithrilsecurity.io/contact" target="_blank" rel="noopener noreferrer" class="rounded-2xl px-8 block py-3 text-center h-11 text-white hover:bg-gray-600" style="width: 100%;">
 					Help
 					</a>
-					<a href="https://1qdag6eehid.typeform.com/to/EFrGfL1u" target="_blank" rel="noopener noreferrer" class="px-8 block text-center h-11 py-3 text-white hover:bg-gray-600" style="width: 100%;">
+					<a href="https://1qdag6eehid.typeform.com/to/EFrGfL1u" target="_blank" rel="noopener noreferrer" class="rounded-2xl px-8 block text-center h-11 py-3 text-white hover:bg-gray-600" style="width: 100%;">
 					Give Feedback
 					</a>
-					<a href="https://www.mithrilsecurity.io/faq" target="_blank" rel="noopener noreferrer" class="block text-center h-11 py-3 text-white hover:bg-gray-600" style="width: 100%;">
+					<a href="https://www.mithrilsecurity.io/faq" target="_blank" rel="noopener noreferrer" class="rounded-xl block text-center h-11 py-3 text-white hover:bg-gray-600" style="width: 100%;">
 					FAQ
 					</a>
-					<a href="https://github.com/mithril-security/blind_chat/issues" target="_blank" rel="noopener noreferrer" class="block text-center h-11 py-3 text-white hover:bg-gray-600" style="width: 100%;">
+					<a href="https://github.com/mithril-security/blind_chat/issues" target="_blank" rel="noopener noreferrer" class="rounded-2xl block text-center h-11 pt-3 text-white hover:bg-gray-600" style="width: 100%;">
 					Report an issue
 					</a>
 				</div>
